59519d647a1e5fb6dc2a246dee3d1e06ae6bc299
Luke McDuff <ljm7b2@mail.umkc.edu>
2016-10-24 10:36:22 -0500

    added interfaces for bullet and player, working with one component

--- AsteroidsArch/src/AsteroidsArch/IClock.java
+++ AsteroidsArch/src/AsteroidsArch/IClock.java
@@ -2,12 +2,6 @@ package AsteroidsArch;
 
 public interface IClock {
 	
-	public void setPaused(boolean paused);
-	
-	public boolean isPaused();
-	
-	public void update();
-	
-	public boolean hasElapsedCycle();
+	String SetCyclesPerSecond();
 
 }
