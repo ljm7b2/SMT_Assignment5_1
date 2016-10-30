30707a6dd2d9d08eaa7f3b2a3cf151e284ac5251
Luke McDuff <ljm7b2@mail.umkc.edu>
2016-10-30 12:06:16 -0500

    messup

--- AsteroidsArch/src/AsteroidsArch/IClock.java
+++ AsteroidsArch/src/AsteroidsArch/IClock.java
@@ -1,7 +1,9 @@
 package AsteroidsArch;
 
 public interface IClock {
-	
+	public void setPaused(boolean paused);
+	public boolean isPaused();
+	public void update();
+	public boolean hasElapsedCycle();
 	public String SetCyclesPerSecond();
-
 }
